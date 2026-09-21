f058db8aeb6f1dfb2a41d4f1758d186c2803b122
Shruti Dube <shruti.dube1408@gmail.com>
2026-09-21 22:50:50 +0530

    Time: 3 ms (43.87%) | Memory: 8.5 MB (64.72%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers and numbers ending with 0 (except 0 itself) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+        
+        // When length is odd, middle digit doesn't matter (reversedHalf / 10)
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+};
